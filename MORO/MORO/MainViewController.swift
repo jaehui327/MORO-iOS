01d4a356bfc71a733c200ba547c55d29ab7c9c47
jaehui327 <jaehui327@naver.com>
2018-07-28 19:58:42 +0900

    mainpage

--- MORO/MORO/MainViewController.swift
+++ MORO/MORO/MainViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    //MARK: IBOutlet
+    @IBOutlet weak var HPGuage: UIImageView!
+    @IBOutlet weak var HPHideLeadingConstraint: NSLayoutConstraint!
+    
+    //MARK: properties
+    
+    //MARK: life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +27,5 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //MARK: Methods
 }
